6cd8a9e1ddaab22e0be910349426bf107cc0a6fd
ror <ror@ror.(none)>
2014-08-04 17:24:24 +0530

    left and right

--- app/controllers/signed/posts_controller.rb
+++ app/controllers/signed/posts_controller.rb
@@ -1,6 +1,20 @@
 class Signed::PostsController < Signed::BaseController
 	def index
-		@posts = Post.all
+		@posts = Post.all.reverse
+		@post = Post.new
+
 	end
+	def create
+    @post = current_user.posts.new(post_params)
+    
+    if @post.save
+      redirect_to signed_posts_path
+    else
+      render :index
+    end
+  end
+def post_params
+    params.require(:post).permit(:context)
+  end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731114321) do
+ActiveRecord::Schema.define(version: 20140804111521) do
 
   create_table "posts", force: true do |t|
     t.text     "context"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140731114321) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
